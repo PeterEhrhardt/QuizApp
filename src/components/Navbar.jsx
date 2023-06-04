f6835927f9dc6a59b66d57b0bd82865cead71566
Peter.Ehrhardt <peh014@uib.no>
2023-06-04 15:31:40 +0200

    drop down menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,32 @@ import React from "react";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const handleHallaClick = (click) => {
+    console.log("click");
+  };
+
   return (
     <div id="navbar-container">
       <div id="logo-container"> QuizJizz</div>
       <div id="tema-container">
-        <div>halla</div>
-        <div>på</div>
-        <div>deg</div>
-        <div>tjukken</div>
+        <div
+          id="halla-container"
+          onClick={handleHallaClick}
+          className="navbar-item"
+        >
+          halla
+          <div>
+            <ul id="DropdownMenu">
+              <li>på</li>
+              <li>deg</li>
+              <li>din</li>
+              <li>gris</li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-item">på</div>
+        <div className="navbar-item">deg</div>
+        <div className="navbar-item">tjukken</div>
       </div>
     </div>
   );
