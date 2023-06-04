bb6b9400bfb0618c4f8f96f382270787f5cd1e18
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2023-06-04 19:12:11 +0200

    categories

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
+import Categories from "./components/Categories";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Categories />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,29 +3,15 @@ import React from "react";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
-  const handleHallaClick = (click) => {
-    console.log("click");
-  };
-
   return (
     <div id="navbar-container">
       <div id="logo-container"> QuizJizz</div>
       <div id="tema-container">
-        <div
-          id="halla-container"
-          onClick={handleHallaClick}
-          className="navbar-item">
-          halla
-          <ul id="drop-down-menu">
-            <li>på</li>
-            <li>deg</li>
-            <li>din</li>
-            <li>gris</li>
-          </ul>
+        <div id="halla-container" className="navbar-item">
+          Browse Categories
         </div>
-        <div className="navbar-item">på</div>
-        <div className="navbar-item">deg</div>
-        <div className="navbar-item">tjukken</div>
+        <div className="navbar-item">One minute quiz</div>
+        <div className="navbar-item">Endless Quiz</div>
       </div>
     </div>
   );
