cb9e7e82303cfeaeeddea3cd26e686e861cba988
Peter.Ehrhardt <peh014@uib.no>
2023-06-04 14:46:29 +0200

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,17 @@ import React from "react";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
-  return <div>Tema</div>;
+  return (
+    <div id="navbar-container">
+      <div id="logo-container"> QuizJizz</div>
+      <div id="tema-container">
+        <div>halla</div>
+        <div>på</div>
+        <div>deg</div>
+        <div>tjukken</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
