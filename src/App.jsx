d92d606ea361e5645ad462daf838273263aa262e
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2023-07-09 15:32:40 +0200

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,8 @@ import { useState } from "react";
 
 import "./App.css";
 
-import Navbar from "./components/Navbar";
-import Categories from "./components/Categories";
-
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Categories />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
